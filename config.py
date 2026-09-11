177ababddb93ffa295ea12500d37792dff9b38a0
Pratik04 <pratikghag9@gmail.com>
2026-09-12 02:08:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'change-this-in-production')
