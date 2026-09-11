b0cf5f4124f8faadd182e730a95751fd1039d462
Pratik04 <pratikghag9@gmail.com>
2026-09-12 02:13:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from config import Config
 from models import db
 from routes.products import products_bp
 from routes.orders import orders_bp
+from routes.auth import auth_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app)
     db.init_app(app)
+    JWTManager(app)
 
     app.register_blueprint(products_bp)
     app.register_blueprint(orders_bp)
+    app.register_blueprint(auth_bp)
 
     return app
 
